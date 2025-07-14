b0a126f83ea156c34edb8ace267ec3e5eaf7e4bf
Brett Longworth <blongworth@whoi.edu>
2025-07-14 15:28:54 -0400

    update schema

--- locness_datamanager/setup_db.py
+++ locness_datamanager/setup_db.py
@@ -9,22 +9,22 @@ def setup_sqlite_db(db_path):
     # Enable WAL mode for concurrency
     cur.execute('PRAGMA journal_mode=WAL;')
 
-    # Create fluorometer table
+    # Create rhodamine table
     cur.execute('''
-        CREATE TABLE IF NOT EXISTS fluorometer (
-          timestamp INTEGER PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS rhodamine (
+          datetime_utc INTEGER PRIMARY KEY,
           latitude REAL, 
           longitude REAL, 
           gain INTEGER, 
           voltage REAL, 
-          concentration REAL
+          rho_ppb REAL
         );
     ''')
 
     # Create ph table
     cur.execute('''
         CREATE TABLE IF NOT EXISTS ph (
-            pc_timestamp TEXT PRIMARY KEY,
+            datetime_utc INTEGER PRIMARY KEY,
             samp_num INTEGER,
             ph_timestamp TEXT, 
             v_bat REAL,
@@ -50,7 +50,7 @@ def setup_sqlite_db(db_path):
     # Create tsg table
     cur.execute('''
         CREATE TABLE IF NOT EXISTS tsg (
-            timestamp INTEGER PRIMARY KEY,
+            datetime_utc INTEGER PRIMARY KEY,
             scan_no INTEGER,
             cond REAL,
             temp REAL,
@@ -62,16 +62,26 @@ def setup_sqlite_db(db_path):
         );
     ''')
 
+    # Create gps table
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS tsg (
+            datetime_utc INTEGER PRIMARY KEY,
+            nmea_time_utc INTEGER,
+            latitude REAL,
+            longitude REAL
+        );
+    ''')
+
     # Create resampled_data table
     cur.execute('''
         CREATE TABLE IF NOT EXISTS resampled_data (
             timestamp INTEGER PRIMARY KEY,
-            lat REAL,
-            lon REAL,
-            rhodamine REAL,
+            latitude REAL,
+            longitude REAL,
+            rho_ppb REAL,
             ph REAL,
-            temp REAL,
-            salinity REAL,
+            temp_c REAL,
+            salinity_psu REAL,
             ph_ma REAL
         );
     ''')
